6c4545e37c5ea7dec924637fd896328f60db26b4
TheGuardianWolf <TheGuardianWolf@outlook.co.nz>
2019-10-01 07:29:40 +1300

    Fix for typos

--- wpa_monitor.py
+++ wpa_monitor.py
@@ -12,13 +12,14 @@ def network_speed():
     try:
         s = speedtest.Speedtest()
         s.get_best_server()
-        return s.download()
+        speed = s.download()
+        return speed
     except speedtest.ConfigRetrievalError:
         return 0.0
 
 
 def reconnect(interface):
-    subprocess.run(["systemctl", "restart", "wpa_supplicant@{interface}".format(interface)])
+    return subprocess.check_output(["systemctl", "restart", "wpa_supplicant@{}".format(interface)])
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Monitors wpa network connection and reconnects on fail or slow speeds")
@@ -29,12 +30,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     try:
+        interface = args.interface
+        interval = int(args.interval)
         threshold = int(args.threshold)
         while True:
-            time.sleep(args.interval)
+            time.sleep(interval)
             speed = network_speed()
+            print(threshold)
+            print(speed)
             if speed < threshold:
                 print("Reconnecting network due to low speeds ({} < {})".format(speed, threshold))
-                reconnect(args.interface)
+                print(reconnect(interface))
     except Exception as e:
         traceback.print_exc()
